af32d78ec2f01716a6f4fce120683f6dd7ec397f
Djelloul Abid <djelloulabid75@gmail.com>
2024-11-05 23:44:45 +0100

    récupération projet

---
+++ Devspace/budget-buddy/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container mt-4">
+      <h2>Welcome</h2>
+      <p>"Budget Buddy helps you visualize your income and expenses, allowing you to plan your finances smartly and confidently."</p>
+      <p>
+        <strong>Start now !</strong>
+      </p>
+    </div>
+  );
+}
+
+export default Home;
